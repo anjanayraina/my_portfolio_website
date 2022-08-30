f0508318890291c62a5cc5a2da5d6fb2371d2707
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-30 16:23:27 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/Navbar.css'
+function Navbar() {
+  return (
+    <div className = "NavbarWrapper">
+    <div className ="NavbarTotal">
+        <div >Home</div>
+        <div>About Me!!</div>
+        <div>Projects</div>
+        <div>Contact Me</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar.js';
+function HomePage() {
+  return (
+    <div>
+
+      <Navbar />
+    </div>
+  )
+}
+
+export default HomePage
