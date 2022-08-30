5c193523dd06ff25d6cb51f946850e52f295da89
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-30 17:06:57 +0530

    Name and Animations Added

---
+++ src/Components/Introductoin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/Introductoin.css'
+function Introductoin() {
+  return (
+    <div className = "leftPart">
+    <div className = "backgroundScreenLeft"></div>
+    <div className = "nameAndIntroWrapper">
+    <div className = "name">Anjanay Raina</div>
+    </div>
+    </div>
+  )
+}
+
+export default Introductoin
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar.js';
+import Introductoin from '../Components/Introductoin.js';
 function HomePage() {
   return (
     <div>
 
       <Navbar />
+      <Introductoin />
     </div>
   )
 }
