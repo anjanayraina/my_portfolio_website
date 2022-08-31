f0941653a9c09fc1750577385c9dc38b6f308205
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-31 15:58:54 +0530

    icons Added

--- src/Components/Introductoin.js
+++ src/Components/Introductoin.js
@@ -2,7 +2,9 @@ import React from 'react'
 import '../Styles/Introductoin.css'
 import resume from '../Photos/resume.png'
 import myImage from '../Photos/myImage.png'
-
+import leetcode from '../Photos/leetcode.png'
+import github from '../Photos/Github.png'
+import linkedin from '../Photos/Linkedin.png'
 function Introductoin() {
   return (
     <div className = "fullDivWrapper">
@@ -15,8 +17,12 @@ function Introductoin() {
     <div className = "introText">Front-End Web Developer</div>
     <div className = "introSubText">Hey there!! , I am a Front-End Web and Blockchain Developer focused on creating</div>
     <div className = "introSubText">Useful and Cool Dapps. I am quite proficient in Android App Development as well </div>
-    <img className = "resumeImage" src ={resume}/>
+    {/* <img className = "resumeImage" src ={resume}/> */}
     <div className = "CVtext">Download CV</div>
+    <img src ={leetcode} className = "leetcode" />
+    <div className = "leetcodeText">Leetcode ID</div>
+    <img src ={github} className = "github" />
+    <img src ={linkedin} className = "linkedin" />
     
     </div>
     </div>
