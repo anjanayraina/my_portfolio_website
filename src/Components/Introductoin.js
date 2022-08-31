78503689781c4b7ee2ec39769f003e24081d5f97
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-31 13:58:39 +0530

    Animations Added

--- src/Components/Introductoin.js
+++ src/Components/Introductoin.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../Styles/Introductoin.css'
 import resume from '../Photos/resume.png'
+import myImage from '../Photos/myImage.png'
+
 function Introductoin() {
   return (
     <div className = "fullDivWrapper">
@@ -12,9 +14,16 @@ function Introductoin() {
     <div className = "introText">Full Stack Blockchain Developer &</div>
     <div className = "introText">Front-End Web Developer</div>
     <div className = "introSubText">Hey there!! , I am a Front-End Web and Blockchain Developer focused on creating</div>
-    <div className = "introSubText">Useful and Cool Dapps.I am quite proficient in Android App Development as well </div>
+    <div className = "introSubText">Useful and Cool Dapps. I am quite proficient in Android App Development as well </div>
     <img className = "resumeImage" src ={resume}/>
+    <div className = "CVtext">Download CV</div>
+    
     </div>
+    </div>
+
+    <div className = "rightPart">
+      <img src ={myImage}/>
+      
     </div>
     </div>
   )
