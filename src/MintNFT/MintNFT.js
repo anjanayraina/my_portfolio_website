f9299d295345143351c2a04d318c1e34e081ceae
anjanay <anjanayraina326@gmail.com>
2023-01-01 15:20:02 +0530

    Particles not overlappig now

--- src/MintNFT/MintNFT.js
+++ src/MintNFT/MintNFT.js
@@ -13,7 +13,7 @@ function MintNFT() {
 
     return (
         <Container fluid className="about-section">
-          <Particle />
+        
           <Container>
             <Row style={{ justifyContent: "center", padding: "10px" }}>
               <Col
@@ -24,10 +24,16 @@ function MintNFT() {
                   paddingBottom: "50px",
                 }}
               >
-                <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                  Know about <strong className="purple">My Journey!!</strong>
+                <h1 style={{ fontSize: "4rem", paddingBottom: "20px" }}>
+                  Mint my <strong className="purple">NFT</strong>
                 </h1>
-     
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <Button variant="outline-light">Mint</Button>{' '}
               </Col>
               <Col
                 md={5}
@@ -39,9 +45,7 @@ function MintNFT() {
             </Row>
 
     
-
-    
-    
+            <Particle />
           </Container>
         </Container>
       );
