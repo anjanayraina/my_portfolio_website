1b935181a615a1087844ea7353e8c9ccd0039f67
anjanay <anjanayraina326@gmail.com>
2023-06-30 20:05:04 +0530

    Resume Page 2 added

--- src/Components/Resume/ResumeNew.js
+++ src/Components/Resume/ResumeNew.js
@@ -8,8 +8,6 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-
-
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -22,12 +20,16 @@ function ResumeNew() {
       <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
-   
+        </Row>
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
