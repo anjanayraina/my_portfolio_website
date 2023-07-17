31fb47a626842e6a1762ddd612729293425ee325
anjanay <anjanayraina326@gmail.com>
2023-07-17 16:20:17 +0530

    Reordering of experience

--- src/Components/Experience/ExperienceCard.js
+++ src/Components/Experience/ExperienceCard.js
@@ -10,47 +10,36 @@ function ExperienceCard() {
           <p style={{ textAlign: "justify" }}>
             So my first work started with <strong className='purple'>Android Development</strong>. Some of the work that i did is : 
           </p> 
-          <h1 className='purple-header'> 1) Kidz.fit</h1>
-          <h5>Jan 2021 - May 2021</h5>
+
+          <h1 className='purple-header'> 5) Natwest Group</h1>
+          <h5>May 2023 - July 2023</h5>
           <ul>
             <li className="about-activity">
-              <MdOutlineControlPoint /> Designed the front end for the Kidz.fit App
+              <MdOutlineControlPoint /> Utilize Oracle Fusion’s functionalities to develop visually appealing and user-friendly reports that effectively convey complexinformation in a clear and concise manner. 
             </li>
             <li className="about-activity">
-              <MdOutlineControlPoint /> Helped in creating the backed for the app in boto3
+              <MdOutlineControlPoint />  Gather and analyze data from various sources to generate comprehensive reports, presenting key findings and recommen-dations to support strategic decision-making processes
+ 
             </li>
+
             <li className="about-activity">
-              <MdOutlineControlPoint /> Also did some user testing of the app
-            </li>
-            <li className="about-activity">
-              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Android Studio, AWS .
+              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Blockchain , Solidity , Remix ide .
             </li>
           </ul>
-          <h1 className='purple-header'> 2) Communev</h1>
-          <h5>May 2021 - June 2021</h5>
+
+          <h1 className='purple-header'> 4) DLT Labs</h1>
+          <h5>Jan 2023 - July 2023</h5>
           <ul>
             <li className="about-activity">
-              <MdOutlineControlPoint /> Helped modernize the UI/UX of their App
+              <MdOutlineControlPoint /> Working on creating a Blockchain for TRAI for regulation of network providers . 
             </li>
+
             <li className="about-activity">
-              <MdOutlineControlPoint /> Fixing Bugs
+              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Blockchain , Solidity , Remix ide .
             </li>
-            <li className="about-activity">
-              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> React Native , JavaScript .
-            </li>
-            
           </ul>
 
-
-        </blockquote>
-      </Card.Body>
-      
-    </Card>
-    <Card className="quote-card-view">      
-      <Card.Body className="blockquote mb-0">
-        <blockquote>
-        <p style={{ textAlign: "justify" }}>After that i dabbled in <strong className='purple'>Machine Learning and Deep Learning</strong> a little bit as well as some more tech stacks. After all that i switched to <strong className ="purple">Blockckchain Development</strong> . Some of my work in Blockchain is Below : </p>
-        <h1 className='purple-header'> 3) Chatbuck</h1>
+          <h1 className='purple-header'> 3) Chatbuck</h1>
         <h5>Jan 2022 - Aug 2022</h5>
           <ul>
  
@@ -67,36 +56,52 @@ function ExperienceCard() {
               <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Ethereum , Solidity, Remix IDE ,JavaScript , Android Studio.
             </li>
           </ul>
-          <h1 className='purple-header'> 4) DLT Labs</h1>
-          <h5>Jan 2023 - July 2023</h5>
+
+          <h1 className='purple-header'> 2) Communev</h1>
+          <h5>May 2021 - June 2021</h5>
           <ul>
             <li className="about-activity">
-              <MdOutlineControlPoint /> Working on creating a Blockchain for TRAI for regulation of network providers . 
+              <MdOutlineControlPoint /> Helped modernize the UI/UX of their App
             </li>
-
             <li className="about-activity">
-              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Blockchain , Solidity , Remix ide .
+              <MdOutlineControlPoint /> Fixing Bugs
+            </li>
+            <li className="about-activity">
+              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> React Native , JavaScript .
+            </li>
+            
+          </ul>
+
+          <h1 className='purple-header'> 1) Kidz.fit</h1>
+          <h5>Jan 2021 - May 2021</h5>
+          <ul>
+            <li className="about-activity">
+              <MdOutlineControlPoint /> Designed the front end for the Kidz.fit App
+            </li>
+            <li className="about-activity">
+              <MdOutlineControlPoint /> Helped in creating the backed for the app in boto3
+            </li>
+            <li className="about-activity">
+              <MdOutlineControlPoint /> Also did some user testing of the app
+            </li>
+            <li className="about-activity">
+              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Android Studio, AWS .
             </li>
           </ul>
+       
+
+        </blockquote>
+      </Card.Body>
+      
+    </Card>
+    <Card className="quote-card-view">      
+      <Card.Body className="blockquote mb-0">
+        <blockquote>
+        <p style={{ textAlign: "justify" }}>After that i dabbled in <strong className='purple'>Machine Learning and Deep Learning</strong> a little bit as well as some more tech stacks. After all that i switched to <strong className ="purple">Blockckchain Development</strong> . Some of my work in Blockchain is Below : </p>
+       
           
-          <h1 className='purple-header'> 5) Natwest Group</h1>
-          <h5>May 2023 - July 2023</h5>
-          <ul>
-            <li className="about-activity">
-              <MdOutlineControlPoint /> Utilize Oracle Fusion’s functionalities to develop visually appealing and user-friendly reports that effectively convey complexinformation in a clear and concise manner. 
-            </li>
-            <li className="about-activity">
-              <MdOutlineControlPoint />  Gather and analyze data from various sources to generate comprehensive reports, presenting key findings and recommen-dations to support strategic decision-making processes
- 
-            </li>
-
-            <li className="about-activity">
-              <MdOutlineControlPoint /> <strong className='purple'>Technologies and Tools:</strong> Blockchain , Solidity , Remix ide .
-            </li>
-
-            
-            
-          </ul>
+          
+         
         </blockquote>
       </Card.Body>
       </Card>
