949fe6cbecb4a3e58cdaf6a6d4a9fd4424f7523d
Anjanay Raina <60835814+anjanayraina@users.noreply.github.com>
2024-12-10 09:29:14 +0530

    Resume Updated

--- src/Components/Resume/ResumeNew.js
+++ src/Components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/2020494_AnjanayRaina_Resume.pdf";
+import pdf from "../../Assets/../Assets/AnjanayRainaResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
